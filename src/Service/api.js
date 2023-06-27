f8fa1f2cf25f5beea19cd3e02a8e115aa3483604
Yash3456 <goyalyash311@gmail.com>
2023-06-27 21:26:23 +0530

    fontend

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const url = 'http://localhost:8000';
 
 export const authenticateLogin = async (user) => {
     try {
-        console.log(user);
-        return await axios.post(`${url}/signup`, user)
+        
+        return await axios.post(`${url}/signup`, user);
     } catch (error) {
+       if(error.response){
         console.log('Error while calling login API: ', error);
+       }
     }
 }
