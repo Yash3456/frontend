98b49b29bd3e4accd2b586810538a906518dbb41
Yash3456 <goyalyash311@gmail.com>
2023-08-08 18:32:57 +0530

    changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,21 +4,25 @@ import Search from './Search'
 import Customerbutton from './customerbutton'
 import { Link } from 'react-router-dom'
 import { Menu } from '@mui/icons-material';
-import Logotext from "../../assests/headingtext.png";
-import Logoimage from "../../assests/logodesign.png";
 
 const Styleheader = styled(AppBar)`
 background:#2874f0;
 height:55px;
+position:absolute;
 `
 
 const Boxcomponenet = styled(Link)`
 margin-left:6%;
 line-height:0;
 `
+const Subheading = styled(Typography)`
+font-size:10px;
+font-style:italic;
+`
 const Plusimage = styled('img')({
-  width:40,
-  height:50,
+  width:10,
+  height:10,
+
 })
 
 const Conatinerbox =styled(Box)(({theme})=>({
@@ -51,7 +55,7 @@ const Header = () => {
  } 
 
  const list = () => (
-  <Box style={{ width: 250 }} onClick={handleclose}>
+  <Box style={{ width: 280 }} onClick={handleclose}>
       <List>
           <listItem button>
               <Customerbutton />
@@ -71,9 +75,15 @@ const Header = () => {
         {list()}
         </Drawer> 
 
-        <Boxcomponenet to={"/"} style={{textDecoration:"none",color:"inherit",marginBottom:"10px"}}>
-          <Plusimage src={Logoimage} alt="sub-logo"/>
-          <img src={Logotext} alt="logo" style={{width:"65%"}}/>
+        <Boxcomponenet to={"/"} style={{textDecoration:"none",color:"inherit"}}>
+         <img src={logoURL} alt="logo" style={{width:"35%"}}/>
+         <Box style={{display:'flex'}}>
+          <Subheading>
+            Explore&nbsp; 
+            <Box component="span" style={{color:"#FFE500"}}>Plus</Box>
+          </Subheading>
+          <Plusimage src={subURL} alt="sub-logo"/>
+         </Box>
         </Boxcomponenet>
         <Search/>
         <Conatinerbox>
